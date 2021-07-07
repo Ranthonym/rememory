d39d42c3d1fe7e2ca62a71755c74f73ec09fe9e6
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-07-07 12:56:41 -0400

    create PostMessageModel based on mongoose schema

---
+++ server/src/models/postMessage.ts
@@ -0,0 +1,37 @@
+import { Schema, model, connect } from "mongoose";
+
+interface Post {
+  title: string;
+  message: string;
+  creator: string;
+  tags: string[];
+  selectedfile: string;
+  likeCount: {
+    type: Number;
+    default: 0;
+  };
+  createdAt: {
+    type: Date;
+    default: Date;
+  };
+}
+
+const postSchema = new Schema<Post>({
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const PostMessageModel = model<Post>("PostMessage", postSchema);
+
+export default PostMessageModel;

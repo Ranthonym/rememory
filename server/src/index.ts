48dae11efed176baaefff6ab0fefd236e84f6fc8
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-07-07 11:22:22 -0400

    connect server to mongodb database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,30 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors'
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
+const app = express();
 
-console.log("hello world")
+app.use(express.json());
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//mongodb atlas connect
+const CONNECTION_URL =
+  "mongodb+srv://mernuser:123qwe@cluster0.pfldj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose
+  .connect(CONNECTION_URL, options)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+// prevent warnings in the console
+mongoose.set("useFindAndModify", false);

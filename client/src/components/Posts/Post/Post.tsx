ae36fefe46d5c31f85cd2168edc993d21dca4271
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-07-12 11:53:27 -0400

    create new route and controller for updating posts in database

--- client/src/components/Posts/Post/Post.tsx
+++ client/src/components/Posts/Post/Post.tsx
@@ -16,7 +16,7 @@ import useStyles from "./styles";
 const Post = ({ post }: any) => {
   const classes = useStyles();
 
-  console.log(post);
+  // console.log(post);
   return (
     <Card className={classes.card}>
       <CardMedia
--- server/src/controllers/posts.ts
+++ server/src/controllers/posts.ts
@@ -1,3 +1,4 @@
+import { mongoose } from "mongoose";
 // import { RequestHandler, Request, Response } from "express";
 import PostMessage from "../models/postMessage";
 
@@ -25,4 +26,19 @@ export const createPost = async (req: any, res: any) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req: any, res: any) => {
+  const { id: _id } = req.params;
+
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post matches requested id");
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
 };

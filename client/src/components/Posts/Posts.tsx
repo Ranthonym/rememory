8ced380d39b741e7e930d65d06a378d52ad1ef04
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-07-14 20:45:10 -0400

    fix missing child prop key error

--- client/src/components/Posts/Posts.tsx
+++ client/src/components/Posts/Posts.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Post from "./Post/Post";
 import useStyles from "./styles";
 import { Grid, CircularProgress } from "@material-ui/core";
@@ -18,7 +18,7 @@ const Posts = ({ setCurrentId }: any) => {
       spacing={3}
     >
       {posts.map((post: any) => (
-        <Grid key={posts._id} item xs={12} sm={6}>
+        <Grid key={post._id} item xs={12} sm={6}>
           <Post post={post} setCurrentId={setCurrentId} />
         </Grid>
       ))}

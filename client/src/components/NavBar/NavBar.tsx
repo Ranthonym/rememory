da5b4c44881d780735da13ef2aed664b52171622
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-07-19 13:34:16 -0400

    pre-setup for new feature branch

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AppBar, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 import useStyles from "./styles";
 
@@ -8,13 +9,28 @@ import memories from "../../images/memories.png";
 export const NavBar = () => {
   const classes = useStyles();
 
+  const user = null;
+
   return (
     <div>
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Rememory
-        </Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
+        <div className={classes.brandContainer}>
+          <Typography
+            component={Link}
+            to="/"
+            className={classes.heading}
+            variant="h2"
+            align="center"
+          >
+            Rememory
+          </Typography>
+          <img
+            className={classes.image}
+            src={memories}
+            alt="icon"
+            height="60"
+          />
+        </div>
       </AppBar>
     </div>
   );

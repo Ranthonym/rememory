627f684a2d310272acdea5dd4897c38055b47ca3
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-07-26 17:04:24 -0400

    create auth middleware and insert into routes to check user token  before controllers are called

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+const auth = async (req: any, res: any, next: any) => {
+  try {
+    // get user token from frontend
+    const token = req.headers.authorization.split(" ")[1];
+    const isCustomAuth = token.length < 500; // check if token is own auth or google auth
+    let decodedData: any;
+
+    if (token && isCustomAuth) {
+      decodedData = jwt.verify(token, "testSecretKey");
+
+      req.userId = decodedData?.id;
+    } else {
+      // google auth
+      decodedData = jwt.decode(token);
+      req.userId = decodedData?.sub;
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default auth;

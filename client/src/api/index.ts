08f863b4db0bb304f2e54b3d12fdcc3f4f1b657b
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-07-27 19:35:37 -0400

    update api file with new axios instance for baseURL

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-// const url = "https://rememory-project-api.herokuapp.com/posts";
-const url = "http://localhost:5000/posts";
+const API = axios.create({ baseURL: "http://localhost:5000" });
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost: any) => axios.post(url, newPost);
+// const url = "https://rememory-project-api.herokuapp.com/posts";
+// const url = "http://localhost:5000/posts";
+
+export const fetchPosts = () => API.get("/posts");
+export const createPost = (newPost: any) => API.post("/posts", newPost);
+export const likePost = (id: any) => API.patch(`/posts/${id}/likePost`);
+export const deletePost = (id: any) => API.delete(`/posts/${id}`);
 export const updatePost = (id: any, updatedPost: any) =>
-  axios.patch(`${url}/${id}`, updatedPost);
-export const deletePost = (id: any) => axios.delete(`${url}/${id}`);
-export const likePost = (id: any) => axios.patch(`${url}/${id}/likePost`);
+  API.patch(`/posts/${id}`, updatedPost);

c0985c5981581b4d9ee81f6bc242bbd69c4c8789
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-07-29 17:57:39 -0400

    point baseURL to heroku server

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "https://rememory-project-api.herokuapp.com",
+});
 
 // const url = "https://rememory-project-api.herokuapp.com/posts";
 // const url = "http://localhost:5000/posts";
